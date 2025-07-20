9514ddda0f1c940597482b279095859779404e7d
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-07-21 08:39:25 +0900

    feat: 🎸 TextWidgetの機能拡充

--- src/main/java/io/github/bluesheep2804/unifyhud/widget/TextWidget.java
+++ src/main/java/io/github/bluesheep2804/unifyhud/widget/TextWidget.java
@@ -1,6 +1,7 @@
 package io.github.bluesheep2804.unifyhud.widget;
 
 import io.github.bluesheep2804.unifyhud.api.widget.AbstractBasicWidget;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
@@ -8,9 +9,69 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.CommonColors;
 
+import java.util.Arrays;
+
 import static io.github.bluesheep2804.unifyhud.UnifyHud.rl;
 
 public class TextWidget extends AbstractBasicWidget {
+    private String prefix = "";
+    private String suffix = "";
+    private Style[] style = {Style.NORMAL};
+    private String color = "#FFFFFF";
+    private boolean dropShadow = true;
+
+    public String getPrefix() {
+        return this.prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getSuffix() {
+        return this.suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public Style[] getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style[] style) {
+        this.style = style;
+    }
+
+    public ChatFormatting[] convertStyleToChatFormatting() {
+        return Arrays.stream(style).map(Style::getFormat).toArray(ChatFormatting[]::new);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getColorInt() {
+        try {
+            return Integer.parseInt(color.replace("#", ""), 16);
+        } catch (NumberFormatException e) {
+            return CommonColors.WHITE;
+        }
+    }
+
+    public boolean hasDropShadow() {
+        return dropShadow;
+    }
+
+    public void setDropShadow(boolean dropShadow) {
+        this.dropShadow = dropShadow;
+    }
+
     @Override
     public ResourceLocation getId() {
         return rl("text");
@@ -20,7 +81,8 @@ public class TextWidget extends AbstractBasicWidget {
     public void render(GuiGraphics guiGraphics) {
         if (!isVisible()) return;
         Font font = Minecraft.getInstance().font;
-        Component textComponent = Component.literal(getComponent().resolve().toString());
+        Component textComponent = Component.literal(prefix + getComponent().resolve().toString() + suffix)
+                .withStyle(convertStyleToChatFormatting());
         int originX = (int) (guiGraphics.guiWidth() * this.getOriginPoint().getX());
         int originY = (int) (guiGraphics.guiHeight() * this.getOriginPoint().getY());
         int x = originX - (int) (font.width(textComponent) * this.getOriginPoint().getX());
@@ -30,7 +92,26 @@ public class TextWidget extends AbstractBasicWidget {
                 textComponent,
                 x + getPosX(),
                 y + getPosY(),
-                CommonColors.WHITE
+                getColorInt(),
+                dropShadow
         );
+    }
+
+    public enum Style {
+        NORMAL(ChatFormatting.RESET),
+        BOLD(ChatFormatting.BOLD),
+        ITALIC(ChatFormatting.ITALIC),
+        UNDERLINE(ChatFormatting.UNDERLINE),
+        STRIKETHROUGH(ChatFormatting.STRIKETHROUGH);
+
+        private final ChatFormatting format;
+
+        public ChatFormatting getFormat() {
+            return format;
+        }
+
+        Style(ChatFormatting chatFormat) {
+            this.format = chatFormat;
+        }
     }
 }
